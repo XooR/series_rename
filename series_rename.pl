f4211fdd687126687486582205efc0b28006aa48
Stanislav Antic <stanislav@stanislav-desktop.(none)>
2009-08-30 19:57:56 +0200

    Updated documentation with POD.

--- series_rename.pl
+++ series_rename.pl
@@ -1,23 +1,57 @@
 #!/usr/bin/perl 
-#===============================================================================
-#
-#         FILE:  rename_series.pl
-#
-#        USAGE:  rename_series.pl (<-n name_of_series><-i id_of_series>)\
-#        (--se-regex "(season)(episode)"|-r) [--dummy|-d] file_to_rename...
-#
-#  DESCRIPTION:
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  Stanislav Antic (), stanislav.antic@gmail.com
-#      COMPANY:  ETF
-#      VERSION:  1.0
-#      CREATED:  08/22/2009 08:35:54 PM
-#     REVISION:  ---
-#===============================================================================
+=pod
+
+=head1 NAME
+
+series_rename - Renames files of series with a given pattern.
+
+=head1 SYNOPSIS
+
+B<series_rename> [B<-d>] B<-n> name_of_series B<-r> capture_regex files...
+
+B<series_rename> [B<-d>] B<-i> id_of_series B<-r> capture_regex files...
+
+=head1 DESCRIPTION
+
+series_rename fetches titles of episodes and use them to form a valid
+file name. For now you cannot change the way how it is generating final name
+except if you are editing code directly.
+
+=head1 OPTIONS
+
+=over
+
+=item B<-d>, B<--dummy>
+
+Don't move files. It just show result.
+
+=item B<-r> B<--se-regex>
+
+Regex that specifies how to fetch season and episode infos from file name.
+
+Example:
+
+C<-r "S(\d\d)E(\d\d)"> for files like "Jericho_S03E02" 
+
+=item B<-i> B<--series_id>
+
+Specifies series id of specific series. This could be necessary if name of
+series isn't uniq.
+
+=item B<-n> <--series_name>
+
+Here you are giving name of series. If it isn't uniq it will give you list
+of choices and IDs of series.
+
+=back
+
+=head1 EXAMPLES
+
+$ series_rename -n "Everybody Loves Earl" -r "S(\d\d)E(\d\d)" -d *.avi
+
+$ series_rename -i 79330 -r "S(\d\d)E(\d\d)" -d *.avi
+
+=cut
 
 use strict;
 use warnings;
@@ -38,8 +72,8 @@ my $dont_write  = 0;
 GetOptions(
     "series_name|n=s" => \$series_name,
     "series_id|i=i"   => \$series_id,
-    "se-regex|r=s"   => \$se_regex,
-    "dont-write|d"   => \$dont_write,
+    "se_regex|r=s"    => \$se_regex,
+    "dummy|d"         => \$dont_write,
 );
 
 die "You didn't gave series name!"
@@ -77,6 +111,15 @@ $series_name = $tvdb->getSeriesName($series_id)
 
 FILE:
 for my $file (@ARGV) {
+
+    if ($file =~ m{\Q*.\E # *. part of globing operator
+            [[:alpha:]]+  # [a-zA-Z]+
+            }xms
+            ) {
+# Push files that are given with onto @ARGV and continues
+        push @ARGV, glob $file;
+        next FILE;
+    }
 # Not really safe
     my ($season, $episode) = ($file =~ m/$se_regex/);
 
@@ -104,5 +147,4 @@ for my $file (@ARGV) {
     print $file."->".$new_file_name."\n";
     move($file, $new_file_name)
         unless $dont_write;
-
 }
